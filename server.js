11b4320a0f1b7ce988adadd62ee14d97ca6ce4c8
jmwojcikowski <jakubwojcikowski00@gmail.com>
2024-02-18 21:29:09 +0100

    test .env x2

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const ToDoItem = require('./src/models/toDoList.model')
 
-const mongoURI = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const MONGODB_URI="mongodb+srv://jakubwojcikowski:ofw8Crs6cR0VUD1B@cluster0.w6kqbyi.mongodb.net/ToDoList"
 
 app.use(cors());
 app.use(express.json());
 
-// Połączenie z bazą danych MongoDB Atlas test funkcjonalności TESTTESTTEST
-mongoose.connect(`mongodb://${mongoURI}`, {
+// Połączenie z bazą danych MongoDB Atlas test .env
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Connection to MongoDB failed:', error.message);
 });
 
 
