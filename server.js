97796e4c88d3ab160eeeae3dd2eaf4acbe29ca3a
jmwojcikowski <jakubwojcikowski00@gmail.com>
2024-02-18 18:45:30 +0100

    message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Połączenie z bazą danych MongoDB Atlas test funkcjonalności
+// Połączenie z bazą danych MongoDB Atlas test funkcjonalności TESTTESTTEST
 mongoose.connect(`mongodb://${mongoURI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
